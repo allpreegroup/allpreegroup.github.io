66567e4bc2b230d65ab0c3c74b81acfd2ab70cb7
allpreegroup <101064537+allpreegroup@users.noreply.github.com>
2022-05-05 17:02:04 -0500

    Add files via upload

---
+++ js/intlTel.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+    
+            // Set up phone widget
+            $("#phone").intlTelInput({
+                utilsScript: 'https://www.watsapp.tv/js/utils.js',
+                numberType: "MOBILE",
+                nationalMode: false,
+                autoFormat: true,
+                autoPlaceholder: true
+            });
+    
+            // Click on button send
+            $('#addContact').submit(function (event) {
+                event.preventDefault();
+                var formData = $("#addContact").serializeArray();
+                $('#submitButton').prop('disabled', true);
+                $('.form-error').hide();
+    
+                // Convert the form data to backend format
+                var requestData = {};
+                formData.map(function (element) {
+                    requestData[element.name] = element.value;
+                });
+    
+                // Send post to API
+		$.ajax({
+                url: 'https://hooks.zapier.com/hooks/catch/113581/o61bzz5/',
+                type: "GET",
+                beforeSend: function (request) {
+                    // request.setRequestHeader("Access-Control-Allow-Origin", "*");
+                },
+                // dataType: 'json',
+                // contentType: 'application/json',
+                data: requestData,
+                success: function (result) {
+                    // You can see the result from the console
+                    // tab of the developer tools
+                    // console.log(result);
+                    $('.custom-form').hide();
+                    $('.form-success').show();
+                },
+                error: function (xhr, resp, text) {
+                    $('.form-error').show();
+                    $('#submitButton').prop('disabled', false);
+                }
+	        });
+            });
+        });
