ff56a5938c8f99332d09b8e6edc42044f642f4ca
allpreegroup <101064537+allpreegroup@users.noreply.github.com>
2025-03-31 16:57:01 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ var preLoad = function(){
   console.log("Installing web app");
   return caches.open("offline").then(function(cache) {
     console.log("caching index and important routes");
-    return cache.addAll(["/balance/", "/vouchers/", "/"]);
+    return cache.addAll(["/bal", "/"]);
   });
 };
 
