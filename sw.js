386023edcbeb2f51ccf394dad6d10fa21daca7bb
allpreegroup <101064537+allpreegroup@users.noreply.github.com>
2025-03-31 17:29:55 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ var preLoad = function(){
   console.log("Installing web app");
   return caches.open("offline").then(function(cache) {
     console.log("caching index and important routes");
-    return cache.addAll(["/bal", "/"]);
+    return cache.addAll(["/"]);
   });
 };
 
@@ -42,7 +42,7 @@ var returnFromCache = function(request){
   return caches.open("offline").then(function (cache) {
     return cache.match(request).then(function (matching) {
      if(!matching || matching.status == 404) {
-       return cache.match("offline.html");
+       return cache.match("404.html");
      } else {
        return matching;
      }
