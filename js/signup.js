852ccb84705a108c95214264395037153414d515
allpreegroup <101064537+allpreegroup@users.noreply.github.com>
2025-05-13 20:47:59 -0500

    Update signup.js

--- js/signup.js
+++ js/signup.js
@@ -8,7 +8,7 @@ function init_signup() {
   const loader = document.getElementById("loading-modal");
   const hiddenIframe = document.getElementById("hidden_iframe");
 
-  let iframeInitialized = false;
+//  let iframeInitialized = false;
   window.submitted = false;
 
   const savedUser = localStorage.getItem("signedUpUser");
@@ -100,13 +100,6 @@ function init_signup() {
 
 hiddenIframe.onload = () => {
   console.log("iframe loaded");
-
-  // First iframe load (page load), just mark it initialized
-  if (!iframeInitialized) {
-    iframeInitialized = true;
-    console.log("First iframe load, init only");
-    return;
-  }
 
   // Only handle submit after first load is done
   if (window.submitted) {
